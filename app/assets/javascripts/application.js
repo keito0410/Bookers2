e74d7801cbb5da4be14d1259f17903cbd054d5ce
keito0410 <041019keito@gmail.com>
2020-05-04 11:31:00 +0900

    [Add] Bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
 //= require turbolinks
+//= require_jquery
+//= require bootstrap-sprockets
 //= require_tree .
