d58b2f83f3a355a34ab5f45f00b908ecd1f2444f
vagrant <vagrant@localhost.localdomain>
2020-05-04 01:39:24 +0000

    [Add] 詳細ページ、編集機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,12 +13,19 @@ class BooksController < ApplicationController
   end
 
   def create
-  	@book = Book.new(book_params)
-    @book.save
+  	book = Book.new(book_params)
+    book.save
     redirect_to books_path
   end
   
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   private
